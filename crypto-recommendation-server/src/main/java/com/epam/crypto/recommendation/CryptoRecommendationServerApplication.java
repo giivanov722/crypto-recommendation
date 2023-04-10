6c099b08f6bf70590d7a57a21b156d32d6d78ce4
giivanov722 <georgiivanonv828@gmail.com>
2023-04-10 10:54:08 +0300

    Add initial project

---
+++ crypto-recommendation-server/src/main/java/com/epam/crypto/recommendation/CryptoRecommendationServerApplication.java
@@ -0,0 +1,13 @@
+package com.epam.crypto.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoRecommendationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoRecommendationServerApplication.class, args);
+	}
+
+}
---
+++ crypto-recommendation-server/src/test/java/com/epam/crypto/recommendation/CryptoRecommendationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.crypto.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoRecommendationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
